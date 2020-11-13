ac72a49b4170c447fbd63a3189d838a09a3bf878
Tom B <thomas.brisson@ecl17.ec-lyon.fr>
2020-11-13 15:13:49 +0100

    fix __str__ of classes and add other models to admin

--- agency/admin.py
+++ agency/admin.py
@@ -4,5 +4,13 @@ from django.contrib import admin
 
 
 # permet d'ajouter la table Ride au site admin
-from .models import Ride
+from .models import Ride, Reduction, Client, Reservation, Car, Train, Station, Place
+
 admin.site.register(Ride)
+admin.site.register(Reduction)
+admin.site.register(Client)
+admin.site.register(Reservation)
+admin.site.register(Car)
+admin.site.register(Train)
+admin.site.register(Station)
+admin.site.register(Place)
