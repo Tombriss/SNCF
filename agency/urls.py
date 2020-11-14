e975e0440f72a45d78165e41f52c6124c54da147
Amaury de Barbuat <amaury.debarbuat@gmail.com>
2020-11-14 12:42:50 +0100

    updates

--- agency/urls.py
+++ agency/urls.py
@@ -3,5 +3,34 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # je ne sais pas si on doit laisser une procédure initiale... ?
     path('', views.index, name='index'),
+
+    # page d'accueil permettant de choisir si nouveau client ou client existant, et de consulter les réservations
+    path('agency/homepage/', views.homepage, name='homepage'),
+
+    # le nouveau client renseigne ses informations
+    # le nc fait une recherche de trajet
+    # le nc visualise les trajets dispos
+    # le nc choisis son trajet et son ticket apparait
+    path('agency/homepage/newclient/', views.newclient, name='newclient'),
+    path('agency/homepage/newclient/ridesearch/', views.nc_ridesearch, name='nc_ridesearch'),
+    path('agency/homepage/newclient/ridesearch/availablerides', views.nc_availablerides, name='nc_availablerides'),
+    path('agency/homepage/newclient/ridesearch/availablerides/ticket', views.nc_ticket, name='nc_ticket'),
+
+    # le client existant entre son identifiant
+    # le ec fait une recherche de trajet
+    # le ec visualise les trajets dispo
+    # le ec choisis son trajet et son ticket apparait
+    path('agency/homepage/existingclient/', views.existingclient, name='existingclient'),
+    path('agency/homepage/existingclient/ridesearch/', views.ec_ridesearch, name='ec_ridesearch'),
+    path('agency/homepage/existingclient/ridesearch/availablerides', views.ec_availablerides, name='ec_availablerides'),
+    path('agency/homepage/existingclient/ridesearch/availablerides/ticket', views.ec_ticket, name='ec_ticket'),
+
+    # le client renseigne son identifiant
+    # il accède alors à sa liste de réservation
+    path('agency/homepage/bookedrides/', views.bookedrides, name='bookedrides'),
+    path('agency/homepage/bookedrides/listofrides', views.listofrides, name='listofrides')
+
 ]
--- agency/views.py
+++ agency/views.py
@@ -1,9 +1,50 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
-from django.http import HttpResponse
-
-
 def index(request):
     return HttpResponse("Hello, world. You're at the agency index.")
+
+def homepage(request):
+    return render(request, 'agency/homepage.html')
+
+
+
+def newclient(request):
+    return render(request, 'agency/newclient.html')
+
+def nc_ridesearch(request):
+    return render(request, 'agency/nc_ridesearch.html')
+
+def nc_availablerides(request):
+    return render(request, 'agency/nc_availablerides.html')
+
+def nc_ticket(request):
+    return render(request, 'agency/nc_ticket.html')
+
+
+
+def existingclient(request):
+    return render(request, 'agency/existingclient.html')
+
+def ec_ridesearch(request):
+    return render(request, 'agency/ec_ridesearch.html')
+
+def ec_availablerides(request):
+    return render(request, 'agency/ec_availablerides.html')
+
+def ec_ticket(request):
+    return render(request, 'agency/ec_ticket.html')
+
+
+
+
+def bookedrides(request):
+    return render(request, 'agency/bookedrides.html')
+
+def listofrides(request):
+    return render(request, 'agency/listofrides.html')
