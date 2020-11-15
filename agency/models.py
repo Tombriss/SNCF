adbc6419326a41e1d9ea0146df59a315c9ade8da
Amaury de Barbuat <amaury.debarbuat@gmail.com>
2020-11-15 19:31:26 +0100

    update tables de relation

--- agency/models.py
+++ agency/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-
 class Reduction(models.Model):
 
     # création d'un menu déroulant, correctement réalisée ?
@@ -13,7 +12,12 @@ class Reduction(models.Model):
     REDUCTION_CARD_CHOICES = [(NO_CARD, 'no reduction'), (JUNIOR_CARD, 'junior reduction'), (SENIOR_CARD, 'senior reduction')]
     reduction_card_type = models.CharField(max_length=2, choices=REDUCTION_CARD_CHOICES, default=NO_CARD)
 
-    # il reste à définir la variable de pourcentage de la réduction, disons 30% pour les jeunes et 20% pour les seniors
+    # valeur de la réduction : 30% pour les jeunes et 20% pour les seniors
+    NO_REDUCTION = 0
+    JUNIOR_REDUCTION = 0.3
+    SENIOR_REDUCTION = 0.2
+    PERCENTAGES = [(NO_REDUCTION, '0%'), (JUNIOR_REDUCTION, '30%'), (SENIOR_REDUCTION, '20%')]
+    reduction_percentage = models.FloatField(choices=PERCENTAGES, default=NO_REDUCTION)
 
     def __str__(self):
         return self.reduction_card_type
@@ -22,9 +26,9 @@ class Reduction(models.Model):
 
 class Client(models.Model):
 
-    prenom = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
-    id_client = models.IntegerField()
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    id_client = models.CharField(max_length=200, blank=True)
 
     # création d'un menu déroulant, correctement réalisée ?
     NO_CARD = 'NC'
@@ -33,25 +37,31 @@ class Client(models.Model):
     REDUCTION_CARD_CHOICES = [(NO_CARD, 'no reduction'), (JUNIOR_CARD, 'junior reduction'), (SENIOR_CARD, 'senior reduction')]
     reduction_card_type = models.CharField(max_length=2, choices=REDUCTION_CARD_CHOICES, default=NO_CARD)
 
+    reduction_card_type = models.ForeignKey(Reduction, on_delete=models.CASCADE)
+
     def __str__(self):
         return str(self.id_client)
 
 
 
-class Reservation(models.Model):
+class Train(models.Model):
 
-    price_ticket = models.FloatField()
-    place_number = models.IntegerField()
-    id_reservation = models.IntegerField()
-
-    # à définir avec la bonne syntaxe correspond à la relation souhaitée :
-    # ForeignKey (FK) pour id_ride
-    # ForeignKey (FK) pour id_voiture
-    # ForeignKey (FK) pour id_client
+    id_train = models.IntegerField(blank=True)
 
     def __str__(self):
-        return str(self.id_reservation)
+        return str(self.id_train)
 
+
+
+
+class Station(models.Model):
+
+    id_station = models.IntegerField(blank=True)
+    name_station = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.id_station)
 
 
 
@@ -60,45 +70,52 @@ class Car(models.Model):
     number_available_places_car = models.IntegerField()
     number_reserved_places_car = models.IntegerField()
     car_number = models.IntegerField()
-    id_car = models.IntegerField()
+    id_car = models.IntegerField(blank=True)
 
-    # FK pour train_number
+    # FK pour id_train
+    id_train = models.ForeignKey('Train', on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.id_car)
 
 
-class Train(models.Model):
-
-    train_number = models.IntegerField()
-
-    def __str__(self):
-        return str(self.train_number)
 
 
 class Ride(models.Model):
 
-    id_ride = models.IntegerField()
+    id_ride = models.IntegerField(blank=True)
     price_ride = models.FloatField()
-    departure = models.DateTimeField()
-    arrival = models.DateTimeField()
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
 
-    # FK pour train_number
+    # FK pour id_train
     # FK pour id_gare_départ
     # FK pour id_gare_arrivée
+    id_train = models.ForeignKey('Train', on_delete=models.CASCADE)
+    departure_station = models.ForeignKey('Station', on_delete=models.CASCADE, related_name='departure')
+    arrival_station = models.ForeignKey('Station', on_delete=models.CASCADE, related_name='arrival')
 
     def __str__(self):
         return str(self.id_ride)
 
 
-class Station(models.Model):
+class Reservation(models.Model):
 
-    id_station = models.IntegerField()
-    name_station = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
+    price_ticket = models.FloatField()
+    place_number = models.IntegerField()
+    id_reservation = models.IntegerField()
+
+    # vérifier que les foreign keys ont du sens :
+    # ForeignKey (FK) pour id_ride
+    # ForeignKey (FK) pour id_voiture
+    # ForeignKey (FK) pour id_client
+
+    id_ride = models.ForeignKey('Ride', on_delete=models.CASCADE)
+    id_car = models.ForeignKey('Car', on_delete=models.CASCADE)
+    id_client = models.ForeignKey('Client', on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.id_station)
+        return str(self.id_reservation)
 
 
 class Place(models.Model):
@@ -107,6 +124,8 @@ class Place(models.Model):
     number_available_places_train = models.IntegerField()
 
     # FK pour id_trajet
+    id_ride = models.ForeignKey('Ride', on_delete=models.CASCADE)
+
 
     def __str__(self):
         return str(self.id_place)
